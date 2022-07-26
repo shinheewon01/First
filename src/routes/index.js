53c697d0b5019faa0487d7ade5b564bff35d89cb
shinheewon01 <shw20231@gmail.com>
2022-07-26 18:22:15 +0900

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage';
+// import SignupPage from '@/views/SignupPage';
+
+Vue.use(VueRouter);
+export default new VueRouter({
+	mode: 'history',
+	// 웹 주소 안에 #을 없애줌!
+	routes: [
+		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
+			path: '/login',
+			component: () => import('@/views/LoginPage.vue'),
+		},
+		{
+			path: '/signup',
+			component: () => import('@/views/SignupPage.vue'),
+		},
+		{
+			path: '*',
+			component: () => import('@/views/NotFoundPage.vue'),
+		},
+	],
+});
